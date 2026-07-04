23976033e39cbc62d54b9d8bbf14ca7362a9f2c2
GreatestCoder <namansharma9858581009@gmail.com>
2026-07-05 05:18:14 +0530

    debugging

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+console.log("NEXT_PUBLIC_API_URL =", process.env.NEXT_PUBLIC_API_URL);
+
 const apiClient = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8001",
     headers: { "Content-Type": "application/json" }, timeout: 180000,
